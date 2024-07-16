62b4859923fedc95c8cc4274faaec446506ce3f0
Dwarkesh29032001 <dwarkeshvirkhare@gmail.com>
2024-07-17 00:05:54 +0530

    simple HTTP server using Express

---
+++ week-2/Http_server/index.js
@@ -0,0 +1,12 @@
+
+//How to create a simple Http Sever using Express 
+const express = require('express')
+const  app = express();
+const port = 3000
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+})
+
+app.listen(port);
+console.log(`Listening to the Port Number ${port} `);
